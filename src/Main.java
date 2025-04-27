2b30b07683f052882cf23280296c21aac8fd157c
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 16:46:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import Company.Car;
+import Company.Ford;
+import Company.Holden;
+import Company.Mitsubishi;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car(8, "Base car");
+
+        printStartEngine(car);
+
+        System.out.println(car.accelerate());
+
+        System.out.println(car.brake());
+
+        Car mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+
+        printStartEngine(mitsubishi);
+
+        System.out.println(mitsubishi.accelerate());
+
+        System.out.println(mitsubishi.brake());
+
+        Car ford = new Ford(6, "Ford Falcon");
+
+        printStartEngine(ford);
+
+        System.out.println(ford.accelerate());
+
+        System.out.println(ford.brake());
+
+        Car holden = new Holden(6, "Holden Commodore");
+
+        printStartEngine(holden);
+
+        System.out.println(holden.accelerate());
+
+        System.out.println(holden.brake());
+    }
+
+    private static void printStartEngine(Car car) {
+        System.out.println(car.startEngine());
     }
 }
